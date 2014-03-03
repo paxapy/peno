513bc2b47bb212686af2f2a30307f5d597184b0f
paxapy <paxapy@gmail.com>
2014-03-04 01:40:30 +0400

    gitignore, restore media serve

--- peno/urls.py
+++ peno/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url, static
 from django.contrib import admin
+
 
 
 admin.autodiscover()
@@ -8,4 +10,4 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
-)
+) + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
