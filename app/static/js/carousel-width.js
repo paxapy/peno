788534ddfd875e323ecac7bc997a3de814582c64
paxapy <paxapy@gmail.com>
2014-03-08 15:52:13 +0400

    carousel fix

---
+++ app/static/js/carousel-width.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+	$('.carousel').carousel({
+    	pause: "false",
+    	interval: 4000
+	});
+
+	$('.carousel').css({'margin': 0, 'width': $(window).outerWidth()});
+	$('.carousel .item').css({'width': '100%'});
+	$('.carousel-inner div.item img').each(function() {
+		var imgSrc = $(this).attr('src');
+		$(this).parent().css({'background': 'url('+imgSrc+') center center no-repeat', '-webkit-background-size': '100% ', '-moz-background-size': '100%', '-o-background-size': '100%', 'background-size': '100%', '-webkit-background-size': 'cover', '-moz-background-size': 'cover', '-o-background-size': 'cover', 'background-size': 'cover'});
+		$(this).remove();
+	});
+
+	$(window).on('resize', function() {
+
+		$('.carousel').css({'width': $(window).outerWidth()});
+	});
+}); 
